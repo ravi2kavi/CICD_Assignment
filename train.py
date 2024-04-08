d4d8d7f61e7f65b67a98381295a3d811ac6e39d6
ravi1994kavi <93464757+ravi1994kavi@users.noreply.github.com>
2024-04-08 07:04:48 +0530

    Using RandomForestClassifier

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -11,10 +11,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
-model = SVC().fit(X_scaled, y)
+model = RandomForestClassifier().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
