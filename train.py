d0623735da663472fbca326129dfc2fbb9ac23eb
ravi1994kavi <93464757+ravi1994kavi@users.noreply.github.com>
2024-04-08 06:45:06 +0530

    Using SVC in place of regression in train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
+from sklearn.svm import SVC
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -9,7 +10,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+model = SVC().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
