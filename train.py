6b5baecfb8e2196627ba8cb977d65e9ba4ac0a66
ravi1994kavi <93464757+ravi1994kavi@users.noreply.github.com>
2024-04-08 07:12:04 +0530

    GradientBoostingClassifier

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import pickle
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -11,7 +13,12 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = RandomForestClassifier().fit(X, y)
+#model = RandomForestClassifier().fit(X, y)
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+model = GradientBoostingClassifier().fit(X_scaled, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
