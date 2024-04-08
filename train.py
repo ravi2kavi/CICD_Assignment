ad3198e37611710e3676081c4e3f10d88986788d
ravi1994kavi <93464757+ravi1994kavi@users.noreply.github.com>
2024-04-08 07:28:46 +0530

    Introducing error in train.py

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
-#import numpy as np
+import numpy as np
+
+assert False, "This script intentionally fails"
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -9,7 +11,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-#model = LogisticRegression().fit(X, y)
+model = LogisticRegression().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
