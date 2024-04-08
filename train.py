b208a2291a6213b025804d9094c904ee096609b9
ravi1994kavi <93464757+ravi1994kavi@users.noreply.github.com>
2024-04-08 06:55:44 +0530

    Adding StandardScaler in train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -10,7 +11,10 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = SVC().fit(X, y)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+model = SVC().fit(X_scaled, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
